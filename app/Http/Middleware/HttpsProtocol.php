1482c77a2be1ac194fcba82fe6ea45d6615631b5
Connor Tumbleson <connor.tumbleson@gmail.com>
2017-09-07 08:10:46 -0400

    force ssl on prod

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HttpsProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && !\App::isLocal()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
