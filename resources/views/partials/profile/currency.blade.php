6a3068c43aa0516341e15643c208b8e1e04e4a6b
Connor Tumbleson <connor.tumbleson@gmail.com>
2017-09-12 06:26:47 -0400

    currencies become popovers w/ descriptions
    Signed-off-by: Connor Tumbleson <connor.tumbleson@gmail.com>

--- resources/views/partials/profile/currency.blade.php
+++ resources/views/partials/profile/currency.blade.php
@@ -4,7 +4,9 @@
  */
 ?>
 
-<div class="item" data-toggle="tooltip" title="<?= e($currency->definition->display->name)?>" data-identifier="<?=e($currency->itemHash)?>">
+<div class="item" data-toggle="popover" title="<?= e($currency->definition->display->name)?>"
+     data-identifier="<?=e($currency->itemHash)?>"
+     data-content="<?= e($currency->definition->display->description); ?>">
     <div class="icon"><img src="<?= bungie($currency->definition->display->icon)?>" /></div>
     <div class="value"><?= n($currency->quantity) ?></div>
 </div>
