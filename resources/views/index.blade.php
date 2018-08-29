66acfc07cf74c11d155ead5c13c06cdc61e4f0b4
Connor Tumbleson <connor.tumbleson@gmail.com>
2018-08-28 20:11:53 -0400

    feat: add message about degraded service

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
 
 @extends('layout')
 @section('content')
+    <div class="alert alert-info">
+        The website <a target="_blank" href="https://github.com/Bungie-net/api/wiki/2.3.0-and-2.3.1-Changes-(Forsaken-Release)#breaking-changes">will be degraded</a> until 9/4 with Destiny2 release v2.3.1
+    </div>
     <h2>Weekly Milestones</h2>
     @include('partials.homepage.weekly', ['milestones' => $milestoneHandler->weeklys])
 
