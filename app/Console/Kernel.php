01fd2baa0ff5babced329cfe7908bb063af299f0
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-08-15 12:57:59 -0400

    fix: remove more traces of bugsnag

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,28 +7,15 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
     protected $commands = [
-        \Bugsnag\BugsnagLaravel\Commands\DeployCommand::class,
         \App\Console\Commands\DestinyManifestCommand::class,
-        \App\Console\Commands\DestinyVersionCommand::class,
         \App\Console\Commands\DestinyMedalCommand::class,
         \App\Console\Commands\DestinyGrantVeteranMedalCommand::class,
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param \Illuminate\Console\Scheduling\Schedule $schedule
-     *
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
+        //
     }
 
     /**
