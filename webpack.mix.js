70256d045c49a9b480ab96d0abf477f7666d56b8
Connor Tumbleson <connor.tumbleson@gmail.com>
2020-08-15 07:27:46 -0400

    refactor: flatten "assets" out of directory structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 
-var config = {
+const config = {
     node: "node_modules/"
 };
 
@@ -21,9 +21,9 @@ mix
         config.node + 'bootstrap-3-typeahead/bootstrap3-typeahead.js',
         config.node + 'jquery.cookie/jquery.cookie.js'
     ], 'public/js/vendor.js')
-    .js('resources/assets/js/app.js', 'public/js')
-    .js('resources/assets/js/bootstrap.js', 'public/js')
-    .js('resources/assets/js/destiny.js', 'public/js')
-    .sass('resources/assets/sass/destiny.scss', 'public/css')
-    .copyDirectory('resources/assets/img', 'public/img')
+    .js('resources/js/app.js', 'public/js')
+    .js('resources/js/bootstrap.js', 'public/js')
+    .js('resources/js/destiny.js', 'public/js')
+    .sass('resources/sass/destiny.scss', 'public/css')
+    .copyDirectory('resources/img', 'public/img')
     .version();
